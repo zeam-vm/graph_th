edda5ceb536fd7ba5622f11fd53bcceb5c2c967b
Susumu Yamazaki <zacky1972@gmail.com>
2020-08-11 15:28:56 +0900

    implement Digraph.add_vertice

--- lib/graph_th/digraph.ex
+++ lib/graph_th/digraph.ex
@@ -12,4 +12,12 @@ defmodule Digraph do
   def has_arc?(g, {v1, v2}) when is_struct(g) and is_atom(v1) and is_atom(v2) do
     Map.get(g.arcs, v1) |> Enum.any?(& &1 == v2)
   end
+
+  def add_vertice(g, v) when is_struct(g) and is_atom(v) do
+    if has_vertice?(g, v) do
+      g
+    else
+      %Digraph{arcs: Map.put(g.arcs, v, [])}
+    end
+  end
 end
