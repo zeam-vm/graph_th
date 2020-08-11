4a0f9eb2dc285279fa24720c03385e9927285087
Susumu Yamazaki <zacky1972@gmail.com>
2020-08-11 15:23:42 +0900

    implement Digraph.has_arc?

--- lib/graph_th/digraph.ex
+++ lib/graph_th/digraph.ex
@@ -8,4 +8,8 @@ defmodule Digraph do
   def has_vertice?(g, v) when is_struct(g) and is_atom(v) do
     Map.has_key?(g.arcs, v)
   end
+
+  def has_arc?(g, {v1, v2}) when is_struct(g) and is_atom(v1) and is_atom(v2) do
+    Map.get(g.arcs, v1) |> Enum.any?(& &1 == v2)
+  end
 end
