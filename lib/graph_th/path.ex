8048152144b2bdda42bef304c19192839192a080
Susumu Yamazaki <zacky1972@gmail.com>
2020-08-11 16:33:27 +0900

    implement GraphTh.Path.length_p

--- lib/graph_th/path.ex
+++ lib/graph_th/path.ex
@@ -8,4 +8,12 @@ defmodule GraphTh.Path do
   def is_simple?(path) when is_struct(path) do
     length(Enum.uniq(path.path)) == length(path.path)
   end
+
+  def length_p(%GraphTh.Path{path: []}) do
+    0
+  end
+
+  def length_p(path) when is_struct(path) do
+    length(path.path) - 1
+  end
 end
