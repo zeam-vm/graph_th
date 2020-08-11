35deccf980a117e09ac8c1706c89d48f8f8fbe65
Susumu Yamazaki <zacky1972@gmail.com>
2020-08-11 16:33:13 +0900

    implement GraphTh.Path.is_simple?

--- lib/graph_th/path.ex
+++ lib/graph_th/path.ex
@@ -4,4 +4,8 @@ defmodule GraphTh.Path do
   def empty() do
     %GraphTh.Path{path: []}
   end
+
+  def is_simple?(path) when is_struct(path) do
+    length(Enum.uniq(path.path)) == length(path.path)
+  end
 end
