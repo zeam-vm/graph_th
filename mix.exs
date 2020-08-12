55147e762cb79d4122088cd08e249b37d90cf436
Susumu Yamazaki <zacky1972@gmail.com>
2020-08-13 08:33:43 +0900

    install ex_doc

--- mix.exs
+++ mix.exs
@@ -26,7 +26,9 @@ defmodule GraphTh.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 
   defp description() do
